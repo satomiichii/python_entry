4ce6e51e6de2f354942c0180079dff495e8acad8
Satomi Ichii <sichii@gmail.com>
2021-04-26 16:29:18 -0400

    day 25 completed!

--- 25_states_game/prompt.md
+++ 25_states_game/prompt.md
@@ -1,8 +1,15 @@
 # Day 25 : States Game
 
+create a game that user guesses 50 states in the United States.
+If user make a correct guess, the name of the states appears on the map.
+
 ## My Solution
+
+https://github.com/satomiichii/python_entry/blob/master/25_states_game/script.py
 
 ## Today's Takeaway
 
 - 'csv' is the Python standard module to manipulate csv data
 - 'pandas' is the Python's data analysis library
+- pandas has a lot of method that allows you to easily manipulate csv data, like grouping with specific columns values and count data etc..
+- That was a good way to learn states names and spell
--- 25_states_game/script.py
+++ 25_states_game/script.py
@@ -1,34 +1,59 @@
-with open('weather_data.csv') as data:
-    contents = data.readlines()
-    print(contents)
-
-# import csv
-#
-# with open('weather_data.csv') as data_file:
-#     temp = []
-#     data = csv.reader(data_file)
-#     for row in data:
-#         if row[1] != 'temp':
-#             temp.append(int(row[1]))
-#     print(temp)
-
+import turtle
 import pandas
 
-data = pandas.read_csv('weather_data.csv')
-# print(data['day'])
+IMAGE = 'blank_states_img.gif'
+STATE = 'state'
+X = 'x'
+Y = 'y'
 
-# print(data['temp'].max())
+screen = turtle.Screen()
+screen.title('U.S. States Game')
+screen.setup(700, 550)
+screen.addshape(IMAGE)
+turtle.shape(IMAGE)
 
-# print(data[data.temp == data['temp'].max()])
+data = pandas.read_csv('50_states.csv')
+state_list = data[STATE].tolist()
 
-monday = data[data.day == 'Monday']
-print("fahrenheit", ((monday.temp * 9/5) + 32))
+score = 0
 
-data_dict = {
-    'student': ['Sami', 'Tama', 'Neko'],
-    'score': [80, 95, 100]
-}
 
-new_data = pandas.DataFrame(data_dict)
-print(new_data)
-new_data.to_csv('new_data.csv')
+def check_answer(user_answer):
+    state_name = user_answer.title()
+
+    if state_name in state_list:
+        state_list.remove(state_name)
+        state_x = float(data[data[STATE] == state_name][X])
+        state_y = float(data[data[STATE] == state_name][Y])
+        new_state = turtle.Turtle()
+        new_state.hideturtle()
+        new_state.penup()
+        new_state.goto(state_x, state_y)
+        new_state.write(state_name, align='left', font=('Arial', 10, 'normal'))
+        return True
+
+
+while len(state_list) > 0:
+    answer = screen.textinput(title=f'{score}/50 States correct',
+                              prompt="What's another state's name?")
+
+    if answer == 'exit':
+        new_data = pandas.DataFrame(state_list)
+        new_data.to_csv('state_to_learn.csv')
+        break
+
+    if check_answer(answer):
+        score += 1
+
+    if score == 50:
+        game_is_on = False
+        complete = turtle.Turtle()
+        complete.hideturtle()
+        complete.pencolor('red')
+        complete.penup()
+        complete.goto(0, 250)
+        complete.write('Congrats! You completed 50 states!', align='center', font=('Arial', 15, 'normal'))
+        break
+
+
+screen.mainloop()
