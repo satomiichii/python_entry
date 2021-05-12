a8403a59c29134cc5a99ed8bd250b5d8ce6bb908
Satomi Ichii <sichii@gmail.com>
2021-05-11 20:49:17 -0400

    Completed day 31!

--- 31_flash_card/main.py
+++ 31_flash_card/main.py
@@ -1,31 +1,74 @@
+import random
 from tkinter import *
+import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+# --------- Flash Card Data ---------- #
+try:
+    words_data = pandas.read_csv('./data/words_to_learn.csv')
+except FileNotFoundError:
+    words_data = pandas.read_csv('./data/french_words.csv')
+
+word_dict = words_data.to_dict(orient='records')
+
+random_word = {}
+
+
+def pick_random_word():
+    global flip, random_word
+    window.after_cancel(flip)
+    random_word = random.choice(word_dict)
+    canvas.itemconfig(card_image, image=card_image_front)
+    canvas.itemconfig(language, text='French', fill='black')
+    canvas.itemconfig(word, text=random_word['French'], fill='black')
+
+    flip = window.after(3000, flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_image, image=card_image_back)
+    canvas.itemconfig(language, text='English', fill='white')
+    canvas.itemconfig(word, text=random_word['English'], fill='white')
+
+
+# --------- Remove a word you already remember ---------- #
+
+def word_remembered():
+    word_dict.remove(random_word)
+    save_data = pandas.DataFrame(word_dict)
+    save_data.to_csv('./data/words_to_learn.csv', index=False)
+    pick_random_word()
 # --------- Create UI ---------- #
+
 
 window = Tk()
 window.title('Flash Card')
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip = window.after(3000, flip_card)
+
 # Card
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_image_back = PhotoImage(file='./images/card_back.png')
 card_image_front = PhotoImage(file='./images/card_front.png')
-canvas.create_image(400, 263, image=card_image_back)
-canvas.create_image(400,263, image=card_image_front)
+card_image = canvas.create_image(400,263, image=card_image_front)
 
-language = canvas.create_text(400, 150, text='Language', font=('Arial', 40, 'italic'))
-word = canvas.create_text(400, 263, text='Word', font=('Arial', 60, 'bold'))
+language = canvas.create_text(400, 150, text='', font=('Arial', 40, 'italic'))
+word = canvas.create_text(400, 263, text='', font=('Arial', 60, 'bold'))
 
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Buttons
 wrong_image = PhotoImage(file='./images/wrong.png')
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=pick_random_word)
 wrong_button.grid(column=0, row=1)
 
 right_image = PhotoImage(file='./images/right.png')
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command=word_remembered)
 right_button.grid(column=1, row=1)
+
+pick_random_word()
+
+
 window.mainloop()
--- 31_flash_card/prompt.md
+++ 31_flash_card/prompt.md
@@ -2,7 +2,14 @@
 
 Create a GUI flash card app using Tkinter
 implement following functionalities.
--
+
+- User can see a French word on the flash card
+- After 3 second, the card flips and show the English word that corresponding the French word
+- If user already remember the word, user can click the check button, so that the word is removed from the word list
+- If user is still not sure about the word, user can click the cross button to move on to a next word, while the word is still remaining in the word list 
+- The list of the remaining words is saved as a csv file
+- If user has the csv that stores remaining words, the app restores tha data from the file when use run the app next time
+- If use doesn't have the remaining words csv, the app restores the data from the original words csv
 
 ## My Solution
 
@@ -10,3 +17,4 @@ https://github.com/satomiichii/python_entry/blob/master/31_flash_card/main.py
 
 ## Today's Takeaway
 - This is a capstone project for concreting concepts like Tkinter, pandas, file manipulation, error handling etc.
+- Python doesn't have a concept of hoisting? I have to look up for that
