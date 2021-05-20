0669f4642e0b7f9107889000ca2afb81e9dfb8b7
Satomi Ichii <sichii@gmail.com>
2021-05-20 10:36:25 -0400

    setup day 36

--- 35_rain_alert/main.py
+++ 35_rain_alert/main.py
@@ -6,8 +6,8 @@ from twilio.rest import Client
 load_dotenv()
 
 params = {
-    'lat': '40.712776',
-    'lon': '-74.005974',
+    'lat': '38.627003',
+    'lon': '-90.199402',
     'exclude': 'current,minutely,daily',
     'appid': os.environ['API_KEY']
 }
@@ -18,10 +18,13 @@ print(response.status_code)
 data = response.json()
 print(data)
 
+is_rain = False
+
 for i in range(12):
     main_weather = data['hourly'][i]['weather'][0]['id']
     if main_weather < 700:
         print(f"It will rain in {i} hours")
+        is_rain = True
         break
 
 
@@ -29,11 +32,13 @@ account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
-message = client.messages \
-                .create(
-                     body="It's going to rain today. Remember to bring umbrella ☂️",
-                     from_=os.environ['SENDER_NUMBER'],
-                     to=os.environ['TO_NUMBER']
-                 )
+if is_rain:
+    message = client.messages \
+                    .create(
+                         body="It's going to rain today. Remember to bring umbrella ☂️",
+                         from_=os.environ['SENDER_NUMBER'],
+                         to=os.environ['TO_NUMBER']
+                     )
 
-print(message.sid)
+    print(message.status)
+
--- 35_rain_alert/prompt.md
+++ 35_rain_alert/prompt.md
@@ -1,5 +1,18 @@
 # Day 35 : Rain Alert
 
+Create a Rain Alert App that send you text message if it will rain in next 12 hours.
+- Use Weather API
+- Use Twilio to send SMS message
+- Use Environment valuable
+
 ## My solution
 
+https://github.com/satomiichii/python_entry/blob/master/35_rain_alert/main.py
+
 ## Today's Takeaway
+
+- Sometimes you need API key to access to secure API endpoint
+- You can send SMS message with Python code by using Twilio API
+- you can create environment valuable with either cli command 'export KEY=VALUE' or adding .env file to your project root directory
+- If you accidentally push API key to github, you can remove the commit by following this link
+https://stackoverflow.com/questions/448919/how-can-i-remove-a-commit-on-github
