746db23ebea36a089d504fe38319b080abc36fe0
Satomi Ichii <sichii@gmail.com>
2021-05-26 18:18:12 -0400

    feat: fetch flight search data

--- 39_40_cheap_flight_finder/flight_search.py
+++ 39_40_cheap_flight_finder/flight_search.py
@@ -7,7 +7,7 @@ import datetime as dt
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
     def __init__(self, destination):
-        self.endpoint = 'tequila-api.kiwi.com/v2/search'
+        self.endpoint = 'https://tequila-api.kiwi.com/v2/search'
         self.from_city = 'NYC'
         self.to_city = destination
         self.from_date = ''
@@ -17,23 +17,25 @@ class FlightSearch:
         load_dotenv()
 
     def get_date(self):
-        tomorrow = (dt.datetime.today() + dt.timedelta(days=1)).strftime(f'%m/%d/%Y')
-        six_months = (dt.datetime.today() + dt.timedelta(days=180)).strftime(f'%m/%d/%Y')
+        tomorrow = (dt.datetime.today() + dt.timedelta(days=1)).strftime(f'%d/%m/%Y')
+        six_months = (dt.datetime.today() + dt.timedelta(days=180)).strftime(f'%d/%m/%Y')
         self.from_date = tomorrow
         self.to_date = six_months
 
     def fetch_flight_data(self):
         headers = {
-            'apikey':os.environ['KIWI_API_KEY']
+            'apikey': os.environ['KIWI_API_KEY']
         }
         for city in self.to_city:
             params = {
-                'fly_from':self.from_city,
+                'fly_from': self.from_city,
                 'fly_to': city,
-                'dateFrom': self.from_date,
-                'dateTo': self.to_date
+                'date_from': self.from_date,
+                'date_to': self.to_date
             }
-            response = requests.get(url=self.endpoint, json=params, headers=headers)
+            print(params)
+            print(headers)
+            response = requests.get(url=self.endpoint, params=params, headers=headers)
             response.raise_for_status()
             self.result_list.append(response.json())
         return self.result_list
